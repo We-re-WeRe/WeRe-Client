df435049654ed7f1bf663303e3802906e3763231
강 성엽 (KangSeongYeop) <44183484+PoeySK@users.noreply.github.com>
2024-03-11 19:19:03 +0900

    FEAT: Text Button Atom 작성 (#11)
    * Update issue templates
    
    * Update issue templates
    
    * add to .gitignore
    
    * feat: clsx 패키지 설치
    
    * remove: package-lock.json 삭제
    
    * feat: #4 textButton atom 제작
    
    css 방식에 대한 상세한 방식을 정하지 못해 기능적 요소만 작성
    
    * feat: classname 부분 추가 및 파일&폴더 구조 변경
    
    * feat: Text Button atom 작성

---
+++ were-client/src/components/atoms/TextButton/index.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from "react";
+import clsx from "clsx";
+import Link from "next/link";
+import styles from "./index.module.scss";
+
+interface Props {
+    children: ReactNode;
+    onClick?: () => void;
+    disabled?: boolean;
+    type?: 'submit' | 'reset' | 'button';
+    link?: string;
+    size: 'small' | 'medium' | 'large';
+}
+
+const TextButton = ({ children, onClick, disabled, type, link, size }: Props) => {
+    if (link) {
+        return (
+            <Link
+                href={link}
+                className={clsx(styles.textButton, styles[size ?? 'medium'])}
+            >
+                {children}
+            </Link>
+        );
+    }
+
+    return (
+        <button
+            type={type}
+            disabled={disabled}
+            className={clsx(styles.textButton, styles[size ?? 'medium'])}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
+
+export default TextButton;
