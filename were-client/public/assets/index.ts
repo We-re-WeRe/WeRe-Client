5ec2aeb2c56be16605c72f726d3c286b127d4fb2
채승규 <37896060+csk6314@users.noreply.github.com>
2024-03-14 18:34:08 +0900

    feat: Webtoon Box, IconText Component 구현 및 svg 세팅 (#27)
    * feat: like,review,star svg 파일 추가 및 컴포넌트화
    
    * feat: IconText Component 구현
    
    * feat: webtoonBox Component 구현
    
    * chore: WebtoonBox 이미지 margin 추가 & 작가 Textbutton size 변경경
    
    * feat: Icon 컴포넌트 구현 (IconText)
    
    * chore: webtoonBox css 제목, 작가 글자만 클릭되게 수정
    
    * chore: textButton 롤백
    
    ---------
    
    Co-authored-by: 강 성엽 (KangSeongYeop) <44183484+PoeySK@users.noreply.github.com>

---
+++ were-client/public/assets/index.ts
@@ -0,0 +1,5 @@
+import IconLike from './like.svg';
+import IconReview from './review.svg';
+import IconStar from './star.svg';
+
+export { IconLike, IconReview, IconStar };
---
+++ were-client/src/components/molecules/IconText/index.tsx
@@ -0,0 +1,58 @@
+import React, { ReactNode } from 'react';
+import { IconLike, IconReview, IconStar } from '@/../public/assets';
+import NormalText from '../../atoms/NormalText';
+import clsx from 'clsx';
+import styles from './index.module.scss';
+
+type TIcon = 'like' | 'star' | 'review';
+
+interface Props {
+  type: TIcon;
+  text: string | number;
+  size: 'sm' | 'md';
+}
+type TSizeObj = {
+  icon: number;
+  text: 'sm' | 'md';
+};
+
+type TIconTextSize = {
+  [k: string]: TSizeObj;
+  sm: TSizeObj;
+  md: TSizeObj;
+};
+
+const Icon = (type: TIcon, size: number): ReactNode => {
+  switch (type) {
+    case 'like':
+      return <IconLike width={size} height={size} />;
+    case 'review':
+      return <IconReview width={size} height={size} fill="#000000" />;
+    case 'star':
+      return <IconStar width={size} height={size} fill="#F9E000" />;
+    default:
+      return;
+  }
+};
+
+const IconTextSize: TIconTextSize = {
+  sm: {
+    icon: 16,
+    text: 'sm',
+  },
+  md: {
+    icon: 20,
+    text: 'md',
+  },
+};
+
+const IconText = ({ type, text, size }: Props) => {
+  return (
+    <div className={clsx(styles.iconText)}>
+      {Icon(type, IconTextSize[size].icon)}
+      <NormalText size={IconTextSize[size].text}>{text}</NormalText>
+    </div>
+  );
+};
+
+export default IconText;
