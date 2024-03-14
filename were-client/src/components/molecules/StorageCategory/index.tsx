1a8834185435b4376bca0b32609db174d2c54bd1
PoeySK <piz115@naver.com>
2024-03-14 18:37:12 +0900

    fix: link prop으로 받기

--- were-client/src/components/molecules/StorageCategory/index.tsx
+++ were-client/src/components/molecules/StorageCategory/index.tsx
@@ -7,9 +7,10 @@ import TextButton from "@/components/atoms/TextButton";
 interface Props {
     children: ReactNode;
     name: string;
+    link: string;
 }
 
-const StorageCategory = ({ children, name }: Props) => {
+const StorageCategory = ({ children, name, link }: Props) => {
     return (
         <div className={clsx(styles.categoryName, styles[name])}>
             <TitleText
@@ -18,7 +19,7 @@ const StorageCategory = ({ children, name }: Props) => {
             >
                 {children}
             </TitleText>
-            <TextButton size="small" link="search">
+            <TextButton size="small" link={link}>
                 더보기
             </TextButton>
         </div>
