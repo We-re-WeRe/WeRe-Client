d754348cc502dd8a86c13496d39a757e9f353a16
PoeySK <piz115@naver.com>
2024-03-19 22:46:42 +0900

    feat: 유저 페이지 상단부 추가
    마이 페이지와 유저 페이지 프로필 사진 부분을 bool타입인 edit으로 판단

--- were-client/src/components/molecules/ProfileBox/index.tsx
+++ were-client/src/components/molecules/ProfileBox/index.tsx
@@ -1,32 +1,66 @@
-import React from "react";
+'use client'
+import React, { useRef, useState } from "react";
 import styles from "./index.module.scss";
 import clsx from "clsx";
 import Image from "next/image";
 import ProfileButton from "@/components/atoms/ProfileButton";
-import camera from "@/../public/images/camera.png";
+import clickProfile from "@/../public/images/hover-profile.png";
+import defaultProfile from "@/../public/images/default-profile.png";
 
 interface Props {
     imgSrc?: string;
+    edit: boolean;
 }
-/**
- * hover시 흰색커버로 카메라 모양 출력
- * 클릭시 파일 업로드
- */
-const ProfileBox = ({ imgSrc }: Props) => {
-    const onClick = () => {
-        /**파일 업로드 */
+
+const ProfileBox = ({ imgSrc, edit }: Props) => {
+    if (!edit) { // 유저 프로필 사진 부분
+        return (
+            <div className={clsx(styles.profileBox)}>
+                <Image
+                    src={imgSrc ?? defaultProfile}
+                    alt="profile"
+                    width={202}
+                    height={202}
+                    style={{ borderRadius: "50%" }}
+                />
+            </div>
+        );
     }
-    const onHoverProfile = () => {
-        /**카메라 이미지 */
+    // 마이 페이지 프로필 사진 부분
+    const [isHover, setIsHover] = useState(false);
+    const [profileImage, setProfileImage] = useState();
+    const fileInput = useRef(null);
+
+    /**파일 업로드 */
+    const onClickUpload = async (e: any) => {
+        const file = e.target.files[0]; // 이미지 단 1개만 받음
+        if (!file) return;
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+    }
+
+    /**마우스 오버 */
+    const onMouseOverProfile = () => {
+        setIsHover(true);
+    }
+    /**마우스 리브 */
+    const onMouseLeaveProfile = () => {
+        setIsHover(false);
     }
 
     return (
         <div
             className={clsx(styles.profileBox)}
+            onMouseOver={onMouseOverProfile}
+            onMouseLeave={onMouseLeaveProfile}
         >
-            <div style={{ position: "relative" }}>
-                <ProfileButton usage="mypage" imgSrc={imgSrc} />
+            <div className={clsx(styles.profile)}>
+                <ProfileButton usage="mypage" type="button" imgSrc={imgSrc} />
             </div>
+            {isHover && <div className={clsx(styles.hoverProfile)}>
+                <Image src={clickProfile} alt="" width={202} height={202} />
+            </div>}
         </div>
     );
 }
