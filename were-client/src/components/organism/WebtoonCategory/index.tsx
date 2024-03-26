da92f7a89e4f570d9778aa9441f367c53b95ea89
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-03-26 22:29:06 +0900

    feat: WebtoonCategory 컴포넌트 구현

---
+++ were-client/src/components/organism/WebtoonCategory/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import EnterpriseButton from '@/components/molecules/EnterpriseButton';
+import DayFilter from '@/components/molecules/DayFilter';
+import TagList from '@/components/molecules/TagList';
+import clsx from 'clsx';
+import styles from './index.module.scss';
+
+const dummyTags = [
+  {
+    tagName: '로맨스',
+  },
+  {
+    tagName: '판타지',
+  },
+  {
+    tagName: '여캐가이쁜',
+  },
+  {
+    tagName: '무협',
+  },
+  {
+    tagName: '주인공이잘생긴',
+  },
+  {
+    tagName: '학교일진물',
+  },
+  {
+    tagName: '복수극',
+  },
+  {
+    tagName: '아카데미물',
+  },
+  {
+    tagName: '격투',
+  },
+  {
+    tagName: '공포',
+  },
+];
+
+const WebtoonCategory = () => {
+  return (
+    <div className={clsx(styles.categoryBox)}>
+      <EnterpriseButton />
+      <DayFilter />
+      <div className={clsx(styles.divider)} />
+      <TagList size="small" tags={dummyTags} />
+    </div>
+  );
+};
+
+export default WebtoonCategory;
