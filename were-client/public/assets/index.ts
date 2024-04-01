df7142d3c2a20597340e1677b2e590b228627f57
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-04-01 21:40:34 +0900

    feat: backIcon 추가

--- were-client/public/assets/index.ts
+++ were-client/public/assets/index.ts
@@ -3,5 +3,6 @@ import IconReview from './review.svg';
 import IconStar from './star.svg';
 import IconFollower from './follower.svg';
 import IconMenu from './menu.svg';
+import IconBack from './back.svg';
 
-export { IconLike, IconReview, IconStar, IconFollower, IconMenu };
+export { IconLike, IconReview, IconStar, IconFollower, IconMenu, IconBack };
