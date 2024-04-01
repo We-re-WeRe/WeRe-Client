7b1cb1b4aada1a127a7ab3474d0ee280dd8d9010
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-04-01 21:15:08 +0900

    feat: MenuIcon 추가

--- were-client/public/assets/index.ts
+++ were-client/public/assets/index.ts
@@ -2,5 +2,6 @@ import IconLike from './like.svg';
 import IconReview from './review.svg';
 import IconStar from './star.svg';
 import IconFollower from './follower.svg';
+import IconMenu from './menu.svg';
 
-export { IconLike, IconReview, IconStar, IconFollower };
+export { IconLike, IconReview, IconStar, IconFollower, IconMenu };
