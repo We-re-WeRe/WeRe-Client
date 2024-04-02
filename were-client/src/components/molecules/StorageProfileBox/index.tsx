3782ebc872bd7037fb82610158b7db57ee4e55da
PoeySK <piz115@naver.com>
2024-04-02 18:22:10 +0900

    feat: Storage Profile 구현

---
+++ were-client/src/components/molecules/StorageProfileBox/index.tsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx';
+import Image, { StaticImageData } from 'next/image';
+import React from 'react';
+import NormalText from '@/components/atoms/NormalText';
+import styles from './index.module.scss';
+import IconText from '../IconText';
+
+interface Props {
+  image: string | StaticImageData;
+  name: string;
+  follower: number;
+}
+
+const StorageProfileBox = ({ image, name, follower }: Props) => {
+  return (
+    <div className={clsx(styles.storageProfile)}>
+      <div className={clsx(styles.storageProfileImagePart)}>
+        <Image src={image} alt="profile" width={100} height={100} />
+      </div>
+      <div className={clsx(styles.storageProfileTextPart)}>
+        <NormalText>{name}</NormalText>
+        <IconText type="follower" text={follower} size="sm" />
+      </div>
+    </div>
+  );
+};
+
+export default StorageProfileBox;
