a252cb605f18907559fc75d9d021583e1d38e050
PoeySK <piz115@naver.com>
2024-04-26 16:47:49 +0900

    feat: Review 리스트 기능 구현

--- were-client/src/components/organism/UserpageReviews/index.tsx
+++ were-client/src/components/organism/UserpageReviews/index.tsx
@@ -1,8 +1,8 @@
 import clsx from 'clsx';
 import React from 'react';
 import CategoryTitle from '@/components/molecules/CategoryTitle';
-import ReviewBox from '@/components/molecules/ReviewBox';
 import styles from './index.module.scss';
+import ReviewBoxList from '../ReviewBoxList';
 
 interface ReviewInfo {
   star: number;
@@ -20,22 +20,7 @@ const UserpageReviews = ({ reviews }: Props) => {
   return (
     <div className={clsx(styles.reviews)}>
       <CategoryTitle category="리뷰" count={reviews ? reviews.length : 0} />
-      {reviews ? (
-        <div className={clsx(styles.reviewList)}>
-          {reviews.map(review => (
-            <ReviewBox
-              key={review.link}
-              star={review.star}
-              comment={review.comment}
-              like={review.like}
-              thumbnail={review.thumbnail}
-              link={review.link}
-            />
-          ))}
-        </div>
-      ) : (
-        <div className={clsx(styles.emptyReviews)}>리뷰가 없습니다.</div>
-      )}
+      <ReviewBoxList reviews={reviews} />
     </div>
   );
 };
