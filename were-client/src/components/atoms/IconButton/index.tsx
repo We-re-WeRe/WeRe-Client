0272cced3936cfa4a4579281a1acb63ed838ca67
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-05-07 17:40:32 +0900

    chore: IconButton search type 추가

--- were-client/src/components/atoms/IconButton/index.tsx
+++ were-client/src/components/atoms/IconButton/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Link from 'next/link';
 import clsx from 'clsx';
 import styles from './index.module.scss';
-import { IconBack, IconMenu, IconStar } from '../../../../public/assets';
+import { IconBack, IconMenu, IconSearch, IconStar } from '../../../../public/assets';
 
-type TIcon = 'menu' | 'back' | 'star' | 'blank-star';
+type TIcon = 'menu' | 'back' | 'star' | 'blank-star' | 'search';
 
 interface Props {
   size: number;
@@ -16,13 +16,15 @@ interface Props {
 const Icon = (type: TIcon, size: number) => {
   switch (type) {
     case 'menu':
-      return <IconMenu width={size} height={size} fill="#000000" />;
+      return <IconMenu width={size} height={size} fill="#ffffff" />;
     case 'star':
       return <IconStar width={size} height={size} fill="#F9E000" />;
     case 'blank-star':
       return <IconStar width={size} height={size} fill="#D9D9D9" />;
     case 'back':
       return <IconBack width={size} height={size} fill="#ffffff" />;
+    case 'search':
+      return <IconSearch width={size} height={size} stroke="#ffffff" />;
     default:
       return <IconBack width={size} height={size} fill="#000000" />;
   }
