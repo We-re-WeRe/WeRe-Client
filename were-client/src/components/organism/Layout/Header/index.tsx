27177a4d5ae6d0e7cf2f100d4ac88df05744fbb8
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-05-10 17:20:44 +0900

    feat: Header Component Scroll 기능 구현

--- were-client/src/components/organism/Layout/Header/index.tsx
+++ were-client/src/components/organism/Layout/Header/index.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import clsx from 'clsx';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import NavigationBar from '@/components/molecules/NavigationBar';
 import SearchBar from '@/components/molecules/SearchBar';
 import TextButton from '@/components/atoms/TextButton';
@@ -8,9 +10,36 @@ import styles from './index.module.scss';
 
 const Header = () => {
   const isLogin = true;
+  const throttleTimeout = useRef<NodeJS.Timeout | null>(null);
+  const [transparent, setTransparent] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0 && !transparent) {
+        setTransparent(true);
+        return;
+      }
+      if (window.scrollY === 0 && transparent) {
+        setTransparent(false);
+      }
+    };
+
+    const throttleScroll = () => {
+      if (!throttleTimeout.current) {
+        throttleTimeout.current = setTimeout(() => {
+          handleScroll();
+          throttleTimeout.current = null;
+        }, 50);
+      }
+    };
+
+    window.addEventListener('scroll', throttleScroll);
+    return () => {
+      window.removeEventListener('scroll', throttleScroll);
+    };
+  }, [transparent]);
 
   return (
-    <div className={clsx(styles.headerWrapper)}>
+    <div className={clsx(styles.headerWrapper, { [styles.scrolling]: transparent })}>
       <nav className={clsx(styles.header)}>
         <NavigationBar />
         <div className={clsx(styles.headerRight)}>
