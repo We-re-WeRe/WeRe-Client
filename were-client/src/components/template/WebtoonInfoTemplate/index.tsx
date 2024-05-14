4c860908cdb676f7f81c04528ff458784158482f
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-05-14 16:49:37 +0900

    feat:WebtoonInfoTemplate 컴포넌트 구현

---
+++ were-client/src/components/template/WebtoonInfoTemplate/index.tsx
@@ -0,0 +1,14 @@
+import WebtoonInfo from '@/components/organism/WebtoonInfo';
+import WebtoonInfoContent from '@/components/organism/WebtoonInfoContent';
+import React from 'react';
+
+const WebtoonInfoTemplate = () => {
+  return (
+    <div>
+      <WebtoonInfo />
+      <WebtoonInfoContent />
+    </div>
+  );
+};
+
+export default WebtoonInfoTemplate;
