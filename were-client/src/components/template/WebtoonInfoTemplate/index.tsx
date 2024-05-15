d95e75169be229f61f3e1c899d49a5d4cf29350f
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-05-15 16:54:53 +0900

    chore: Template에 Modal 추가

--- were-client/src/components/template/WebtoonInfoTemplate/index.tsx
+++ were-client/src/components/template/WebtoonInfoTemplate/index.tsx
@@ -1,3 +1,5 @@
+import ReviewModal from '@/components/organism/ReviewModal';
+import StorageListModal from '@/components/organism/StorageListModal';
 import WebtoonInfo from '@/components/organism/WebtoonInfo';
 import WebtoonInfoContent from '@/components/organism/WebtoonInfoContent';
 import React from 'react';
@@ -7,6 +9,8 @@ const WebtoonInfoTemplate = () => {
     <div>
       <WebtoonInfo />
       <WebtoonInfoContent />
+      <ReviewModal />
+      <StorageListModal />
     </div>
   );
 };
