3fd80912e28c9f6573a67a356c58270bd90a395b
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-05-16 18:16:16 +0900

    feat: LoginForm react-hook-form 적용

--- were-client/src/components/organism/LoginForm/index.tsx
+++ were-client/src/components/organism/LoginForm/index.tsx
@@ -1,16 +1,36 @@
+'use client';
+
 import React from 'react';
 import TextButton from '@/components/atoms/TextButton';
 import NormalText from '@/components/atoms/NormalText';
 import LoginInput from '@/components/atoms/LoginInput';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import styles from './index.module.scss';
 
+interface LoginSchema {
+  id: string;
+  pw: string;
+}
+
 const LoginForm = () => {
+  const { handleSubmit, register } = useForm<LoginSchema>();
+
+  const onSubmit: SubmitHandler<LoginSchema> = data => {
+    console.log(data.id, data.pw);
+  };
+
   return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit(onSubmit)}>
       <div className={styles.formWrapper}>
         <div className={styles.idPwWrapper}>
-          <LoginInput id="login_id" labelText="아이디" placeholder="아이디를 입력해주세요." />
-          <LoginInput id="login_password" labelText="비밀번호" placeholder="비밀번호를 입력해주세요" type="password" />
+          <LoginInput {...register('id')} id="login_id" labelText="아이디" placeholder="아이디를 입력해주세요." />
+          <LoginInput
+            {...register('pw')}
+            id="login_password"
+            labelText="비밀번호"
+            placeholder="비밀번호를 입력해주세요"
+            type="password"
+          />
         </div>
         <div className={styles.validWrapper}>
           <NormalText size="xs">Error</NormalText>
@@ -25,7 +45,7 @@ const LoginForm = () => {
         </div>
       </div>
       <div className={styles.loginButtonWrapper}>
-        <button className={styles.loginButton}>
+        <button className={styles.loginButton} type="submit">
           <NormalText>Login</NormalText>
         </button>
       </div>
