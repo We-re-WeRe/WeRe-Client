7103352a0104929614691cee5f9384852d7590f2
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-05-16 23:02:30 +0900

    feat: Auth Layout 구현 & 기존 LoginTemplate Background 삭제

--- were-client/src/components/template/LoginTemplate/index.tsx
+++ were-client/src/components/template/LoginTemplate/index.tsx
@@ -8,19 +8,13 @@ const handjet = Handjet({ weight: '700', subsets: ['latin'] });
 
 const LoginTemplate = () => {
   return (
-    <div className={styles.templateWrapper}>
-      <div className={styles.backgroundImage} />
-
-      <div className={styles.loginTemplate}>
-        <div className={styles.loginWrapper}>
-          <div className={styles.logoWrapper}>
-            <h1 className={handjet.className}>WeRe</h1>
-          </div>
-          <LoginForm />
-          <div className={styles.divider} />
-          <SocialLogins />
-        </div>
+    <div className={styles.loginWrapper}>
+      <div className={styles.logoWrapper}>
+        <h1 className={handjet.className}>WeRe</h1>
       </div>
+      <LoginForm />
+      <div className={styles.divider} />
+      <SocialLogins />
     </div>
   );
 };
