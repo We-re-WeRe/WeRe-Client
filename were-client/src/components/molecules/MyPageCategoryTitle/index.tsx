9bb4c9b20982215ed11598125bafcfb49590af31
PoeySK <piz115@naver.com>
2024-05-28 11:32:56 +0900

    feat: Scroll Side 카테고리 기능 구현

--- were-client/src/components/molecules/MyPageCategoryTitle/index.tsx
+++ were-client/src/components/molecules/MyPageCategoryTitle/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 import TextButton from '@/components/atoms/TextButton';
 import styles from './index.module.scss';
@@ -14,9 +14,37 @@ interface Props {
 }
 
 const MyPageCategoryTitle = ({ tapStates, onClickTitle }: Props) => {
+  const throttleTimeout = useRef<NodeJS.Timeout | null>(null);
+  const [transSide, setTransSide] = useState<boolean>(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 400 && !transSide) {
+        setTransSide(true);
+        return;
+      }
+      if (window.scrollY < 350 && transSide) {
+        setTransSide(false);
+      }
+    };
+
+    const throttleScroll = () => {
+      if (!throttleTimeout.current) {
+        throttleTimeout.current = setTimeout(() => {
+          handleScroll();
+          throttleTimeout.current = null;
+        }, 50);
+      }
+    };
+
+    window.addEventListener('scroll', throttleScroll);
+    return () => {
+      window.removeEventListener('scroll', throttleScroll);
+    };
+  }, [transSide]);
+
   return (
     <div className={clsx(styles.commonCategoryTitle)}>
-      <div className={clsx(styles.title)}>
+      <div className={clsx(styles.title, { [styles.scrollTitle]: transSide })}>
         {tapStates.map((tapState, index) => (
           <div key={tapState.category} className={clsx(styles.tapTitle)}>
             <TextButton
