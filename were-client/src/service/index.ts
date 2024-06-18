74a089094838d47cd87d5c1c6b64151b79282847
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-06-18 16:14:58 +0900

    feat: Interceptor 구현 및 accessToken 및 refreshToken 기능 구현

--- were-client/src/service/index.ts
+++ were-client/src/service/index.ts
@@ -1,18 +1,35 @@
 import axios from 'axios';
+import { getNewAccessToken } from './auth';
+
+let isTokenRefreshing = false;
 
 const apiBe = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10_000,
-  withCredentials: true,
 });
 
 apiBe.interceptors.response.use(
   // Good Response
   response => response,
   // Response Error
-  error => {
+  async error => {
+    console.clear();
     if (axios.isAxiosError(error)) {
-      return Promise.reject(error.response?.data.message);
+      if (error.response?.status === 401) {
+        if (!isTokenRefreshing) {
+          isTokenRefreshing = true;
+          const accessToken = await getNewAccessToken();
+          //  console.log(error.response.config);
+          apiBe.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+            return config;
+          });
+          isTokenRefreshing = false;
+          return apiBe(error.response.config);
+        }
+        return Promise.reject(error);
+      }
+      return Promise.reject(error);
     }
     return Promise.reject(error);
   },
