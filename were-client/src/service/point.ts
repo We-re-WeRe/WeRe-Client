eceffc8037c53d739076f151ddd78eea80df4488
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-06-25 00:10:37 +0900

    feat: Point History 조회 API 구현

--- were-client/src/service/point.ts
+++ were-client/src/service/point.ts
@@ -1,4 +1,5 @@
-import { IPointSum } from '@/types/point';
+import { IPoint, IPointBase, IPointSum } from '@/types/point';
+import { AxiosResponse } from 'axios';
 import apiBe from '.';
 
 export const getPointsSum = async (): Promise<IPointSum> => {
@@ -6,5 +7,15 @@ export const getPointsSum = async (): Promise<IPointSum> => {
     .get(`/points/sum`)
     .then(res => res.data)
     .catch(err => Promise.reject(err));
+  console.log(pointSum);
   return pointSum;
+};
+
+export const getPointHistory = async (): Promise<IPoint> => {
+  const pointHistory = await apiBe
+    .get(`/points/history`)
+    .then(res => res.data)
+    .catch(err => Promise.reject(err));
+  console.log(pointHistory);
+  return pointHistory;
 };
