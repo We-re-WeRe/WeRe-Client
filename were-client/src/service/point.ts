80512a4d5706bfbff8eae08066c578d412816a84
채승규 <37896060+csk6314@users.noreply.github.com>
2024-06-25 00:12:59 +0900

    FEAT: Point API 기능 구현 (#104)
    * chore: IPointBase에서 mount 키 옵셔널로 변경
    
    * feat: Point History 조회 API 구현
    
    * feat: Point 생성 API 구현
    
    * feat: Point 삭제 API 구현

--- were-client/src/service/point.ts
+++ were-client/src/service/point.ts
@@ -1,4 +1,5 @@
-import { IPointSum } from '@/types/point';
+import { IPoint, IPointBase, IPointSum } from '@/types/point';
+import { AxiosResponse } from 'axios';
 import apiBe from '.';
 
 export const getPointsSum = async (): Promise<IPointSum> => {
@@ -6,5 +7,33 @@ export const getPointsSum = async (): Promise<IPointSum> => {
     .get(`/points/sum`)
     .then(res => res.data)
     .catch(err => Promise.reject(err));
+  console.log(pointSum);
   return pointSum;
+};
+
+export const getPointHistory = async (): Promise<IPoint> => {
+  const pointHistory = await apiBe
+    .get(`/points/history`)
+    .then(res => res.data)
+    .catch(err => Promise.reject(err));
+  console.log(pointHistory);
+  return pointHistory;
+};
+
+export const createPoint = async (data: Omit<IPointBase, 'userId'>): Promise<AxiosResponse> => {
+  const response = await apiBe
+    .post(`/points`, data)
+    .then(res => res)
+    .catch(err => Promise.reject(err));
+  console.log(response);
+  return response;
+};
+
+export const deletePoint = async (pointId: number): Promise<AxiosResponse> => {
+  const response = await apiBe
+    .delete(`/points?id=${pointId}`)
+    .then(res => res)
+    .catch(err => Promise.reject(err));
+  console.log(response);
+  return response;
 };
--- were-client/src/types/point.d.ts
+++ were-client/src/types/point.d.ts
@@ -1,6 +1,6 @@
 export interface IPointBase {
   userId: number;
-  mount: number;
+  mount?: number;
   reason: string;
 }
 
