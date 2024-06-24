fa40b34aba1ae6fd034ecded955713bdb56289bd
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-06-24 19:38:48 +0900

    feat: Review API 구현

---
+++ were-client/src/service/review.ts
@@ -0,0 +1,40 @@
+import { IReviewCreate, IReviewPatch, IUserReview } from '@/types/review';
+import apiBe from '.';
+import { AxiosResponse } from 'axios';
+
+export const getReviewsListUser = async (id: number): Promise<IUserReview[]> => {
+  const userReview = await apiBe
+    .get(`/reviews/list/user?ownerId=${id}`)
+    .then(res => res.data)
+    .catch(err => Promise.reject(err));
+  return userReview;
+};
+
+export const createReview = async (body:IReviewCreate): Promise<AxiosResponse> => {
+  const response = await apiBe
+    .post('/reviews',body)
+    .then(res => res)
+    .catch(err => Promise.reject(err));
+  return response;
+};
+
+export const updateReview = async (body:IReviewPatch): Promise<AxiosResponse> => {
+  const response = await apiBe
+    .patch('/reviews',body)
+    .then(res => res)
+    .catch(err => Promise.reject(err));
+  return response;
+};
+
+export const deleteReview = async (reviewId:number): Promise<AxiosResponse> => {
+  const response = await apiBe
+    .delete(`/review?id=${reviewId}`)
+    .then(res => res)
+    .catch(err => Promise.reject(err));
+  return response;
+};
+
+
+
+
+export const B = '';
