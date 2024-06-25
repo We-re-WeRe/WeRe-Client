190a855b1a2df5e0d613e7095378653f0f5458b2
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-06-26 04:18:23 +0900

    feat: QueryClient dehydrate option 추가

--- were-client/src/app/Provider.tsx
+++ were-client/src/app/Provider.tsx
@@ -1,8 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React, { ReactNode } from 'react';
+'use client';
 
-const Provider = ({ children }: Readonly<{ children: ReactNode }>) => {
+import { QueryClient, QueryClientProvider, defaultShouldDehydrateQuery } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+
+const Provider = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   const [queryClient] = React.useState(
     () =>
       new QueryClient({
@@ -11,6 +13,11 @@ const Provider = ({ children }: Readonly<{ children: ReactNode }>) => {
             // With SSR, we usually want to set some default staleTime
             // above 0 to avoid refetching immediately on the client
             staleTime: 60 * 1000,
+          },
+          dehydrate: {
+            // per default, only successful Queries are included,
+            // this includes pending Queries as well
+            shouldDehydrateQuery: query => defaultShouldDehydrateQuery(query) || query.state.status === 'pending',
           },
         },
       }),
