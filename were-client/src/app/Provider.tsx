8f86b52ad7c8010c42ab37912d8146df6026dc26
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-06-25 22:10:19 +0900

    feat: QueryClient 및 QueryClientProvider 세팅

---
+++ were-client/src/app/Provider.tsx
@@ -0,0 +1,27 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React, { ReactNode } from 'react';
+
+const Provider = ({ children }: Readonly<{ children: ReactNode }>) => {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default Provider;
