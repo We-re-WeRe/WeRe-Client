e144abb95025c09e7f13b887e912494c671e889c
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-06-25 22:10:34 +0900

    feat: layout에 Provider 적용

--- were-client/src/app/layout.tsx
+++ were-client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Noto_Sans_KR as NotoSansKR } from 'next/font/google';
 import '../styles/globals.scss';
 import Header from '@/components/organism/Layout/Header';
 import LayoutProvider from './LayoutProvider';
+import Provider from './Provider';
 
 const inter = NotoSansKR({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body>
-        <LayoutProvider>{children}</LayoutProvider>
+        <Provider>
+          <LayoutProvider>{children}</LayoutProvider>
+        </Provider>
         <div id="modal" />
       </body>
     </html>
