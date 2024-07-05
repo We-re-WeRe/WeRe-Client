e2db25fd1c30795a33cf9b5a3b8198eb4f9f183a
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-07-05 15:16:31 +0900

    chore: WebtoonTemplate padding-top 값 및 더미 데이터 삭제

--- were-client/src/components/template/WebtoonListTemplate/index.tsx
+++ were-client/src/components/template/WebtoonListTemplate/index.tsx
@@ -1,223 +1,24 @@
+'use client';
+
+import React from 'react';
+
 import WebtoonCategory from '@/components/organism/WebtoonCategory';
 import WebtoonList from '@/components/organism/WebtoonList';
+
 import clsx from 'clsx';
-import React from 'react';
 import styles from './index.module.scss';
-
-const webtoons = [
-  {
-    title: '임건우',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-
-  {
-    title: '임건우 시즌 2',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test2.png',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 3',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test3.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 4',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 5',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test5.png',
-    link: 'ghd',
-  },
-  {
-    title: '임건우',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-
-  {
-    title: '임건우 시즌 2',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test2.png',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 3',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test3.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 4',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 5',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test5.png',
-    link: 'ghd',
-  },
-  {
-    title: '임건우',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-
-  {
-    title: '임건우 시즌 2',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test2.png',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 3',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test3.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 4',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 5',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test5.png',
-    link: 'ghd',
-  },
-  {
-    title: '임건우',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-
-  {
-    title: '임건우 시즌 2',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test2.png',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 3',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test3.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 4',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 5',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test5.png',
-    link: 'ghd',
-  },
-  {
-    title: '임건우',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-
-  {
-    title: '임건우 시즌 2',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test2.png',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 3',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test3.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 4',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test.jpg',
-    link: 'ghd',
-  },
-  {
-    title: '임건우 시즌 5',
-    author: '채승규',
-    stars: 20,
-    reviews: 129,
-    imageUrl: '/images/test5.png',
-    link: 'ghd',
-  },
-];
+import CombinedBoundary from '@/components/atoms/CombinedBoundary';
+import ErrorFallback from '@/app/webtoon/fallbacks/error';
+import Loading from '@/app/webtoon/fallbacks/loading';
 
 const WebtoonListTemplate = () => {
   return (
     <div className={clsx(styles.webtoonListTemplate)}>
       <div className={clsx(styles.templateWrapper)}>
         <WebtoonCategory />
-        <WebtoonList webtoons={webtoons} />
+        <CombinedBoundary errorFallback={ErrorFallback} suspenseFallback={<Loading />}>
+          <WebtoonList />
+        </CombinedBoundary>
       </div>
     </div>
   );
