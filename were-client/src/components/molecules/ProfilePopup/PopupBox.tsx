5cca9f4eef0c88dab5e80ca2381a41053f5b3565
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-07-18 21:52:56 +0900

    feat: PopupBox 구조 변경 및 로그아웃 기능 추가

--- were-client/src/components/molecules/ProfilePopup/PopupBox.tsx
+++ were-client/src/components/molecules/ProfilePopup/PopupBox.tsx
@@ -1,36 +1,35 @@
-import NormalText from '@/components/atoms/NormalText';
 import React from 'react';
 import clsx from 'clsx';
 import Link from 'next/link';
 import styles from './index.module.scss';
 import IconText from '../IconText';
+import useUserState from '@/hooks/useUserState';
+import { logoutApi } from '@/service/auth';
 
-export type TItem = {
-  link?: string;
-  text: string;
-  clickHandler?: () => void;
-  icon: 'user' | 'logout';
-};
 interface Props {
-  items: TItem[];
   isShow: boolean;
 }
 
-const PopupBox = React.forwardRef<HTMLDivElement, Props>(({ items, isShow }, ref) => {
+const PopupBox = React.forwardRef<HTMLDivElement, Props>(({ isShow }, ref) => {
+  const { clearUser } = useUserState();
+
+  const logoutHandler = async () => {
+    await logoutApi()
+      .then(() => {
+        clearUser();
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     isShow && (
       <div className={clsx(styles.popupWrapper)} ref={ref}>
-        {items.map(item =>
-          item.link ? (
-            <Link key={item.text} href={item.link} className={clsx(styles.popupItem)}>
-              <IconText type={item.icon} text={item.text} size="sm" />
-            </Link>
-          ) : (
-            <div key={item.text} className={clsx(styles.popupItem)} onClick={item.clickHandler} role="presentation">
-              <IconText type={item.icon} text={item.text} size="sm" />
-            </div>
-          ),
-        )}
+        <Link href="/my" className={clsx(styles.popupItem)}>
+          <IconText type="user" text="마이프로필" size="sm" />
+        </Link>
+        <div className={clsx(styles.popupItem)} onClick={logoutHandler} role="presentation">
+          <IconText type="logout" text="로그아웃" size="sm" />
+        </div>
       </div>
     )
   );
