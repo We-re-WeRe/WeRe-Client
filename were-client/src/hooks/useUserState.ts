02e4246ed0a2fa39d1e6f78876ec6408bb0529aa
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-07-18 21:49:56 +0900

    feat: UserState에 Loading state 추가

--- were-client/src/hooks/useUserState.ts
+++ were-client/src/hooks/useUserState.ts
@@ -3,12 +3,16 @@ import { create } from 'zustand';
 
 interface UserState {
   user: IUserBase | null;
+  isLoading: boolean;
+  setLoading: (loading: boolean) => void;
   setUser: (data: IUserBase) => void;
   clearUser: () => void;
 }
 
 const useUserState = create<UserState>(set => ({
   user: null,
+  isLoading: true,
+  setLoading: (loading: boolean) => set({ isLoading: loading }),
   setUser: (data: IUserBase) => set({ user: data }),
   clearUser: () => set({ user: null }),
 }));
