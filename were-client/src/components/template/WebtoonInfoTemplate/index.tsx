e0d5c32d090e9663da8448976fe8c9336128eec7
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-07-29 10:36:08 +0900

    chore: WebtoonInfo Suspense 적용

--- were-client/src/components/template/WebtoonInfoTemplate/index.tsx
+++ were-client/src/components/template/WebtoonInfoTemplate/index.tsx
@@ -1,13 +1,16 @@
 import ReviewModal from '@/components/organism/ReviewModal';
 import StorageListModal from '@/components/organism/StorageListModal';
 import WebtoonInfo from '@/components/organism/WebtoonInfo';
+import { default as InfoLoading } from '@/components/organism/WebtoonInfo/Skeleton';
 import WebtoonInfoContent from '@/components/organism/WebtoonInfoContent';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 const WebtoonInfoTemplate = () => {
   return (
     <div>
-      <WebtoonInfo />
+      <Suspense fallback={<InfoLoading />}>
+        <WebtoonInfo />
+      </Suspense>
       <WebtoonInfoContent />
       <ReviewModal />
       <StorageListModal />
