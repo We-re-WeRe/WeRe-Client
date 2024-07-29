1522553e770ddfb7ad4f49ac602bf8b2aee3d7c8
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-07-29 10:33:42 +0900

    feat: WebtoonInfo API 연동

--- were-client/src/components/molecules/WebtoonInfoDescription/index.tsx
+++ were-client/src/components/molecules/WebtoonInfoDescription/index.tsx
@@ -5,24 +5,27 @@ import TextButton from '@/components/atoms/TextButton';
 import clsx from 'clsx';
 import TagList from '../TagList';
 import styles from './index.module.scss';
+import { getDuplicatedAuthors } from '@/util/author';
+import { ITag } from '@/types/tag';
 
 interface Props {
   title: string;
-  tags: string[];
+  tags: ITag[];
   authors: string[];
   painters: string[];
   description: string;
+  genre: string;
 }
 
-const WebtoonInfoDescription = () => {
-  const { duplicatedAuthors, dAuthors, dPainters } = { duplicatedAuthors: [], dAuthors: [], dPainters: [] };
+const WebtoonInfoDescription = ({ title, tags, authors, painters, description, genre }: Props) => {
+  const { duplicatedAuthors, dAuthors, dPainters } = getDuplicatedAuthors(authors, painters);
 
   return (
     <div className={clsx(styles.webtoonInfoDescription)}>
       <TitleText size="large" color="white">
-        김부장
+        {title}
       </TitleText>
-      <TagList size="medium" type="storage" tags={[]} />
+      <TagList size="medium" type="storage" tags={tags} />
       <div className={clsx(styles.authorSection)}>
         {/** 글/그림 */}
         {duplicatedAuthors.length > 0 && (
@@ -67,15 +70,11 @@ const WebtoonInfoDescription = () => {
         )}
 
         <NormalText color="white" bold>
-          전체 이용가
+          {genre}
         </NormalText>
       </div>
       <NormalText color="white" description>
-        평생 욕심없이 살아온 소심한 윤리교사 전햇살, 그녀의 옆집에 욕망과 쾌락의 신이 이사왔다. 섹시한 외모에 치명적인
-        향기를 내뿜는 그의 이름은 ‘디오니소스’, 줄여서 디오. 디오가 신계 와인을 제조할 때 꼭 들어가야 하는 재료는 인간의
-        평생 이루지 못한 짙은 욕망이다. 그걸 가진 건 바로 햇살! 바라는 거라곤 그저 반복되는 평온한 일상 뿐, 여타 다른
-        꿈이나 욕망이라곤 1도 없이 살아온 햇살인데… 디오는 햇살의 깊은 곳의 욕망을 깨워내기 위해 신의 능력까지
-        사용해가며 그녀를 쉴새없이 귀찮게 굴기 시작한다.
+        {description}
       </NormalText>
     </div>
   );
