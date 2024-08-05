8c543c176aff6ec05f2ed2ee0360440933dbd362
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-08-05 11:35:52 +0900

    feat: useReview에 userReview 관련 옵션, 쿼리 추가

--- were-client/src/hooks/useReview.ts
+++ were-client/src/hooks/useReview.ts
@@ -1,11 +1,17 @@
-import { createReview, getWebtoonReview } from '@/service/review';
-import { IReviewCreate, IWebtoonReview } from '@/types/review';
-import { queryOptions, useMutation, useQueryClient } from '@tanstack/react-query';
+import { createReview, getReviewsListUser, getWebtoonReview } from '@/service/review';
+import { IReviewCreate, IUserReview, IWebtoonReview } from '@/types/review';
+import { queryOptions, useMutation, useQueryClient, useSuspenseQuery } from '@tanstack/react-query';
 
 export const webtoonReviewOptions = (webtoonId: number) =>
   queryOptions({
     queryKey: ['webtoon', webtoonId, 'review'],
     queryFn: (): Promise<IWebtoonReview[]> => getWebtoonReview(webtoonId),
+  });
+
+export const userReviewOptions = (userId: number) =>
+  queryOptions({
+    queryKey: ['user', userId, 'review'],
+    queryFn: async (): Promise<IUserReview[]> => await getReviewsListUser(userId),
   });
 
 export const useCreateReview = (webtoonId: number, successCb: () => void) => {
@@ -23,4 +29,8 @@ export const useCreateReview = (webtoonId: number, successCb: () => void) => {
     },
     onError: err => console.log(`${err} , 다시 시도해주세요.`),
   });
+};
+
+export const useUserReview = (userId: number) => {
+  return useSuspenseQuery(userReviewOptions(userId));
 };
